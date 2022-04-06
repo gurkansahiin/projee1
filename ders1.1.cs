86f7edef88866807b7abd9c287a76fabcd81da47
gurkansahiin <100939542+gurkansahiin@users.noreply.github.com>
2022-04-06 11:05:23 +0300

    Add files via upload

---
+++ ders1.1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //    int yıl;
+            //    yıl = Convert.ToInt32(textBox1.Text);
+
+            //    if (yıl % 4 == 0)
+            //    {
+            //        MessageBox.Show("Artık Yıldır");
+
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("artık değil");
+            //    }
+
+
+            //int x = Convert.ToInt32(textBox1.Text);
+            //int y = Convert.ToInt32(textBox2.Text);
+
+            //if (x > 0 && y > 0)
+            //{
+            //    MessageBox.Show("1. bölge");
+            //}
+            //if (x < 0 && y > 0)
+            //{
+            //    MessageBox.Show("2. bölge");
+            //}
+            //if (x < 0 && y < 0)
+            //{
+            //    MessageBox.Show("3. bölge");
+            //}
+            //if (x > 0 && y < 0)
+            //{
+            //    MessageBox.Show("4. bölge");
+            //}
+
+
+            //int fizik = Convert.ToInt32(textBox1.Text);
+            //int mat = Convert.ToInt32(textBox2.Text);
+            //int kimya = Convert.ToInt32(textBox3.Text);
+            //int toplam = fizik + mat + kimya;
+
+            //if (mat >= 65 && fizik >= 55 && kimya >= 50 && toplam >= 180 || mat + fizik >= 180)
+            //{
+            //    MessageBox.Show("kabul");
+
+            //}
+           
+           
+            //else
+            //{
+            //    MessageBox.Show("red");
+            //}
+    //        int k1 = Convert.ToInt32(textBox1.Text);
+    //        int k2 = Convert.ToInt32(textBox2.Text);
+    //        int k3 = Convert.ToInt32(textBox3.Text);
+
+    //        if (k1==k2 && k1!=k3 || k1==k3 && k1!=k2 ||k2==k3 && k1!=k2)
+    //        {
+
+    //            MessageBox.Show("ikizkenar");
+    //        }
+    //        if (k1==k2 && k2==k3)
+    //{
+    //     MessageBox.Show("eskenar"); 
+    //}
+
+
+            //int sıcaklık = Convert.ToInt32(textBox1.Text);
+
+            //if (sıcaklık<0)
+            //{
+            //    MessageBox.Show("dondurucu");
+            //}
+            //if (sıcaklık>=0 && sıcaklık<10)
+            //{
+            //    MessageBox.Show("çok soğuk");
+            //}
+            //if (sıcaklık >= 10 && sıcaklık < 20)
+            //{
+            //    MessageBox.Show(" soğuk");
+            //}
+            //if (sıcaklık >= 20 && sıcaklık < 30)
+            //{
+            //    MessageBox.Show("normal");
+            //}
+            //if (sıcaklık >= 30 && sıcaklık < 40)
+            //{
+            //    MessageBox.Show("sıcak");
+            //}
+            //if (sıcaklık >= 40 )
+            //{
+            //    MessageBox.Show("çok sıcak");
+            //}
+
+            //char alfabe = Convert.ToChar(textBox1.Text);
+
+            //switch (alfabe)
+            //{
+
+            //    case 'a':
+            //        MessageBox.Show("sesli harf"); break ;
+            //    case 'e':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'o':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'ö':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'ı':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'i':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'u':
+            //        MessageBox.Show("sesli harf"); break;
+            //    case 'ü':
+            //        MessageBox.Show("sesli harf"); break;
+            //    default:
+            //        MessageBox.Show("sessiz harf");
+            //        break;
+            //}
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+            //int kenar, menü, cevre;
+            //kenar = Convert.ToInt32(textBox1.Text);
+            //menü = Convert.ToInt32(textBox2.Text);
+
+            //switch (menü)
+            //{
+            //    case 1:
+            //        cevre = kenar * 4;
+            //        MessageBox.Show("kare çevresi="+cevre.ToString());
+            //        break;
+            //    case 2:
+            //        cevre=kenar*3;
+            //        MessageBox.Show("ücgen çevresi="+cevre.ToString());
+            //        break;
+
+            //    default:
+            //        MessageBox.Show("yanlis deger");
+            //        break;
+            //}      
+        }
+    }
+}
---
+++ menülü işçi sorusu.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       int[] yaslar  = new int[20];
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (int i in yaslar)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                yaslar[i] = rnd.Next(20, 65);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (int i in yaslar)
+                if(i>20 && i<35)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (int i in yaslar)
+                if(i>35 && i>50)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (int i in yaslar)
+                if(i>50 && i<65)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double toplam = 0, ort;
+            foreach (int i in yaslar)
+            {
+                toplam = toplam + i;
+            }
+            ort = toplam / 20;
+
+            MessageBox.Show(ort.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int enb = yaslar[0];
+            int enk = yaslar[0];
+
+            for (int i = 1; i < yaslar.Length; i++)
+            {
+                if (enb < yaslar[i])
+                    enb = yaslar[i];
+                else
+                    enk = yaslar[i];
+            }
+            MessageBox.Show(enb.ToString());
+            MessageBox.Show(enk.ToString());
+        }
+    }
+}
